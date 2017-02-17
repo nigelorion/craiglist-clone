736b53ba60e355279a6c9d8b6426c382d47d8a4e
Nigel Burnett <nigelorion@gmail.com>
2017-02-16 16:42:22 -0800

    Upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
